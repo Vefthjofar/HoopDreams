95ce8c7d0631032fe9f3d2cad158f2d922099892
Elvarorn <elvar16@ru.is>
2019-10-17 13:45:28 +0000

    added types

---
+++ schema/types/index.js
@@ -0,0 +1,9 @@
+const basketballField = require("./BasketballField");
+const player = require("./Player");
+const pickupGame = require("./PickupGame");
+
+module.exports = `
+  ${basketballField}
+  ${player}
+  ${pickupGame}
+`;
