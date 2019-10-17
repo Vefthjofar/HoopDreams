7ed08f77fb5fa898325fd2a060d819eda3104f6c
Birkir K <birkir17@ru.is>
2019-10-17 14:15:27 +0000

    mongoose models

--- data/db.js
+++ data/db.js
@@ -1,17 +1,20 @@
-/*const playerSchema = require("../schema/mongodb/player");
-const pickupGameSchema = require("../schema/mongodb/pickupGame");
-const signupPlayerSchema = require("../schema/mongodb/signupPlayer");*/
+const playerSchema = require("../models/Player");
+const pickupGameSchema = require("../models/PickupGame");
+const signupPlayerSchema = require("../models/SignupPlayer");
+const basketballFieldSchema = require("../models/BasketballField");
 const mongoose = require('mongoose');
 
 var connection = mongoose.createConnection(
-      "mongodb+srv://admin:1234@hoopdreamsdbcluster-82jnz.mongodb.net/hoopdreamsdbcluster",
-  { useNewUrlParser: true,
-unsUnifiedTopology:true }
+  "mongodb+srv://admin:1234@hoopdreamsdbcluster-82jnz.mongodb.net/hoopdreamsdbcluster",
+  {
+    useNewUrlParser: true,
+    unsUnifiedTopology: true
+  }
 );
 
-/*module.exports = {
+module.exports = {
   Player: connection.model("Player", playerSchema),
   PickupGame: connection.model("PickupGame", pickupGameSchema),
   SignupPlayer: connection.model("SignupPlayer", signupPlayerSchema),
-  connection
-}*/
+  BasketballField: connection.model("BasketballField", basketballFieldSchema)
+}
---
+++ models/PickupGame.js
@@ -0,0 +1,9 @@
+const Schema = require("mongoose").Schema;
+
+module.exports = new Schema({
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+    location: { type: String, required: true },
+    registeredPlayers: [{ type: String, required: true }],
+    host: { type: Schema.Types.ObjectId, ref: 'Player', required: true }
+});
