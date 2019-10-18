cb01304ccb67eef534deb6c0094ad7f9da200206
Birkir K <birkir17@ru.is>
2019-10-18 16:02:51 +0000

    db context

--- resolvers/playerResolver.js
+++ resolvers/playerResolver.js
@@ -1,14 +1,14 @@
 const errors = require("../errors");
-const db = require("../data/db");
+//const db = require("../data/db");
 
 module.exports = {
   queries: {
     //query AllPlayers{ allPlayers{id, name}}
-    allPlayers: async (parent, args) => {
+    allPlayers: async (parent, args, {db}) => {
       return (await db.Player.find({})).filter(p => p.deleted === false);
     },
     //query getPlayer{player(id:"5da9d82f8e966429fc2911a5"){id,name}}
-    player: async (parent, args) => {
+    player: async (parent, args, {db}) => {
       const player = await db.Player.findById(args.id);
       if (!player.deleted) {
         return player;
@@ -20,11 +20,11 @@ module.exports = {
   },
   mutations: {
     //mutation addPlayer{createPlayer(input:{name:"Lebron"}){id,name}}
-    createPlayer: async (parent, args) => {
+    createPlayer: async (parent, args, {db}) => {
       return await db.Player.create(args.input);
     },
     //mutation updatePlayer{updatePlayer(id:"5da9d82f8e966429fc2911a5",name:"Birkir"){id,name}}
-    updatePlayer: async (parent, args) => {
+    updatePlayer: async (parent, args, {db}) => {
       return await db.Player.findByIdAndUpdate(
         args.id,
         { name: args.name },
@@ -32,7 +32,7 @@ module.exports = {
       );
     },
     //mutation deletePlayer{deletePlayer(id:"5da9defbfd5e2e28707ccc71"){id,name}}
-    deletePlayer: async (parent, args) => {
+    deletePlayer: async (parent, args, {db}) => {
       return await db.Player.findByIdAndUpdate(
         args.id,
         { deleted: true },
